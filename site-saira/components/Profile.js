3fd4898bf3493b311417a747905795cbcf51d690
Saira Aguiar <142703012+sairaaguiar@users.noreply.github.com>
2025-09-16 00:45:07 +0000

    problema da foto resolvido

--- site-saira/components/Profile.js
+++ site-saira/components/Profile.js
@@ -1,3 +1,10 @@
-export function Profile() {
-  return <img src="https://drive.usercontent.google.com/download?id=14GsusKxO12CF5XfEkD7d9puSW1Kllehh" alt="Saira Aguiar" />;
-}
+// export function Profile() {
+//   // return <img src="https://drive.usercontent.google.com/download?id=14GsusKxO12CF5XfEkD7d9puSW1Kllehh" alt="Saira Aguiar" />;
+// }
+// import minhaImagem from "/img/imagem 3x4.jpg";
+
+export const Profile = () => {
+  return (
+      <img src="/img/imagem 3x4.jpg" alt="Minha foto" />
+  );
+};
